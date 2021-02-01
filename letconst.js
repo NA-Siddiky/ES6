20324a49e7e612f7d49bcb80001d4aadf1f336f2
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-01 19:25:17 +0600

    adding JS

---
+++ letconst.js
@@ -0,0 +1,25 @@
+// const bou = "My Love";
+// // bou = "second love"
+// console.log(bou);
+
+
+// const numbers = [12, 32];
+// numbers[1] = 88;
+// numbers.push(12);
+// console.log(numbers);
+
+// numbers = ["sunny", "salman"] // cant input the string in number array /
+
+// const nayok = { name: "Sakib", phone: 120 };
+
+// let patientName = "Rahim";
+// patientName = "fatema"
+// console.log(patientName);
+
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+    console.log(i);
+}
+console.log(i);
