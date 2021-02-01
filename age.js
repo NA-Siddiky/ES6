d257c663142558beaf8777fb29d905638b507b93
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-01 20:08:15 +0600

    adding functions

---
+++ age.js
@@ -0,0 +1,11 @@
+function createUser(name, age = 18) {
+    const user = {};
+    user.name = "sid";
+    user.name = "siddiky"
+    user.age = 18;
+    user.age = 8;
+    return user;
+    
+}
+console.log(createUser());
+
---
+++ template.js
@@ -0,0 +1,19 @@
+const firstName = "justin"
+const lastName = "Biber"
+const fullName = firstName + " " + lastName + " is a good boy";
+const fullNameES6 = `${firstName} ${lastName} ${10 + 18} IS A GOOD BOY`
+
+// console.log(fullName);
+// console.log(fullNameES6);
+
+
+const multiline = "I love you"
+    + "I miss you"
+    + "I really miss you a lot"
+
+const multilineES6 = `i love u
+i miss you
+i really miss you`
+
+console.log(multiline);
+console.log(multilineES6);
