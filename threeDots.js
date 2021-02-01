4380c7c016ccf0e5b59e29cf7459e4bc356dd09b
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-02 02:03:13 +0600

    adding threeDots

--- threeDots.js
+++ threeDots.js
@@ -1,4 +1,17 @@
 const ages = [12, 11, 5, 15, 10];
 const ages2 = [1, 2, 3, 4];
-const allAges = ages.concat(ages2);
-console.log(allAges);
+const ages3 = [101, 102, 104];
+const allAges = ages.concat(ages2).concat([5005]).concat(ages3);
+// const allAges2 = [ages, ages2, 50005, ages3];
+const allAges2 = [...ages, ...ages2, 50005, ...ages3];
+// console.log(allAges2);
+
+
+const businessMan = 600;
+const minister = 400;
+const amla = 300;
+const maximum = Math.max(businessMan, minister, amla);
+// console.log(maximum);
+const bigger = [500, 300, 100];
+const maximum2 = Math.max(...bigger);
+console.log(maximum2);
