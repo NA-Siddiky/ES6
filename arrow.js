a286320e3785c7f87a65f38d8cbe545c7cfe4ee4
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-01 22:24:23 +0600

    adding functions

---
+++ arrow.js
@@ -0,0 +1,21 @@
+// function doubleIt(num) {         //calling function in 1st formula/
+//     return num * 2
+// }
+
+// const doubleIt = function myFun(num) {           //calling function in 2nd formula/
+//     return num * 2;
+// }
+
+const doubleIt = num => num * 2;         //calling function in 3rd formula " Arrow function"/
+// const result = doubleIt(5)
+// console.log(result);
+
+
+const add = (x, y) => x + y;
+// const adding = add(2, 2);
+// console.log(adding);
+// console.log(add(2, 2))
+
+const result = add(50, 70);
+const result2 = give5();
+console.log(result); 
