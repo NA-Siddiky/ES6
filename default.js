44de162d044883d1a47fcdcd20145366256b60d1
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-01 19:36:55 +0600

    adding functions of backup

---
+++ default.js
@@ -0,0 +1,11 @@
+function add(num1, num2 = 20) {
+    // if(num2 == undefined){       //alternative process1 /
+    //     num2 = 0;
+    // }
+
+    // num2 = num2 || 0;        //alternative process 2 /
+    return num1 + num2;
+}
+
+const total = add(15,);
+console.log(total);
