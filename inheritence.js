60f4b1cfb82545916dd0022396dbd40d2dec4136
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-02 17:39:35 +0600

    addind inheritence function

--- inheritence.js
+++ inheritence.js
@@ -14,33 +14,24 @@ class Child extends Parent {
     }
 }
 
-// class GrandChild extends Child {             //Problem using a function in class/
-//     add = (name1, name2) => name1 + name2;
-//     adding = add("Sakib", "Khan");
+class GrandChild extends Child {             //Problem using a function in class/
+    add = (name1, name2) => name1 + name2;
+    adding = add("Sakib", "Khan");
 
-//     getGrandlName() {
-//         return add;
-//     }
-// }
-
-class Test extends GrandChild{
-
+    getGrandlName() {
+        return add;
+    }
 }
 
+// class Test extends GrandChild{
 
-
-
-
-
-
-
-
+// }
 
 
 
 const baby = new Child("Arnold")
 const baby2 = new Child("Tom cruse");
-// const baby3 = new Child("Sakib Khan")
-console.log(baby.getFullName());
-console.log(baby2);
-// console.log(baby3.getGrandlName());
+const baby3 = new Child("Sakib Khan")
+// console.log(baby.getFullName());
+// console.log(baby2);
+console.log(baby3.getGrandlName());
